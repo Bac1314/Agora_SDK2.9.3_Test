3b2a0662b7c84618b6e9aa469979dd725ca6aebb
bac1314 <bacchenghuang@gmail.com>
2021-07-06 22:20:03 +0800

    Initial Commit

---
+++ Agora_SDK2.9.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Agora_SDK2.9.2
+//
+//  Created by Bac Cheng Huang on 2021/7/6.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Agora_SDK2.9.2Tests/Agora_SDK2_9_2Tests.swift
@@ -0,0 +1,33 @@
+//
+//  Agora_SDK2_9_2Tests.swift
+//  Agora_SDK2.9.2Tests
+//
+//  Created by Bac Cheng Huang on 2021/7/6.
+//
+
+import XCTest
+@testable import Agora_SDK2_9_2
+
+class Agora_SDK2_9_2Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
